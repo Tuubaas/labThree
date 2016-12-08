b64c165b2528c14587bc3e1335241ce75a5aba14
Stina Markan <kontakt@stinamarkan.se>
2016-12-08 17:37:03 +0100

    Lagt till GameUtil och påbörjat göra om GoldModel

--- src/GameModel.java
+++ src/GameModel.java
@@ -6,7 +6,7 @@ import java.awt.Dimension;
  * Constructors of subclasses should initiate matrix elements and additional,
  * game-dependent fields.
  */
-public abstract class GameModel {
+public interface GameModel {
 
 	/** A Matrix containing the state of the gameboard. */
 	private final GameTile[][] gameboardState;
